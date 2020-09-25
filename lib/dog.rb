703e521a8414b9ba652cfd3314c1f82eb8bf7d37
Michael I <miodice3@gmail.com>
2020-09-25 19:27:54 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+
+class Dog 
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+#    @breed = breed, 'Mutt'
+#    @breed = params.fetch(:breed, 'Mutt')
+  end
+  
+  def breed=(breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end
+
+end
